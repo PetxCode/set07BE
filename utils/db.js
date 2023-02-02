6d3eb90ae82e5adbbab5c45134bad8e1c2fb4d0a
PetxCode <brighterdayscodelab@gmail.com>
2023-02-02 06:16:33 -0800

    build

--- utils/db.js
+++ utils/db.js
@@ -3,8 +3,10 @@ const mongoose = require("mongoose");
 const url = "mongodb://localhost/studentsDB";
 const url2 =
   "mongodb+srv://newStudent:newStudent@cluster0.gkpjkup.mongodb.net/studentDB07?retryWrites=true&w=majority";
+const url3 =
+  "mongodb+srv://PeterPan:PeterPan@codelab.eqkgv.mongodb.net/studentDB07?retryWrites=true&w=majority";
 
-mongoose.connect(url2).then(() => {
+mongoose.connect(url3).then(() => {
   console.log("database now connected");
 });
 
